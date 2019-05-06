094dfefa2decb27a63bc44f7cb624173792e7e0e
Locallize <Fillanlee@aol.com>
2019-05-06 12:02:44 -0400

    started game

--- index.js
+++ index.js
@@ -1,13 +1,17 @@
 // ***************************************
 // declare constants needed
 // ***************************************
-const container = document.querySelector('#game-container')
+const gameContainer = document.querySelector('#game-container')
 const hiScoreBtn = document.querySelector('#hi-score-btn')
 const hiScoreTbl = document.querySelector('#hi-score-table')
 const newUserForm = document.querySelector('#create-username')
+const scoresBody = document.querySelector('#hi-score-body')
 const newUser = document.querySelector('#user')
 const userDiffcultySelect = document.querySelector('#user-diffculty')
-
+const currentPlayer = document.querySelector('#current-player')
+const logOutBtn = document.querySelector("#log-out-btn")
+let loggedInUser;
+let keySequenceArray = []
 // *****************************************
 // end of delcaration
 // *****************************************
@@ -15,8 +19,7 @@ const userDiffcultySelect = document.querySelector('#user-diffculty')
 // trying to create username and a game instance at the same time....
 newUserForm.addEventListener('submit', (e) => {
     e.preventDefault()
-    console.log(userDiffcultySelect.value);
-
+    // console.log(userDiffcultySelect.value);
     fetch('http://localhost:3000/api/v1/games', {
             method: 'POST',
             headers: {
@@ -26,41 +29,90 @@ newUserForm.addEventListener('submit', (e) => {
             body: JSON.stringify({
 
                 username: newUser.value,
-                difficulty: userDiffcultySelect.value,
-                high_score: 0,
-                level: 1
+                difficulty: userDiffcultySelect.value
             }),
         })
         .then(res => res.json())
-        .then(console.log)
+        .then(player => {
+            // console.log(player)
+
+            loggedInUser = player.username
+            newUserForm.classList.add('hidden');
+
+            currentPlayer.innerHTML = `
+            <p> current player is ${loggedInUser}</p>
+
+            <h3>${userDiffcultySelect.value} MODE</h3>
+            `
+        })
+    newUserForm.reset()
+    logOutBtn.classList.remove("hidden")
 })
 
 // refactor for post request to db
 // const addUser = () =>{
 // }
 
+// logout and clear current user
+logOutBtn.addEventListener('click', (e) => {
+    newUserForm.classList.remove('hidden');
+    loggedInUser = "";
+    currentPlayer.innerHTML = ""
+    logOutBtn.classList.add("hidden")
+})
 
 // toggle hiscore menu when hiscore btn is clicked
 hiScoreBtn.addEventListener('click', (e) => {
     // ternary -- add/remove hidden class to table
     hiScoreTbl.classList.value.includes('hidden') ? hiScoreTbl.classList.remove("hidden") : hiScoreTbl.classList.add("hidden");
+
+    if (hiScoreTbl.classList.value.includes('hidden') === false) {
+        scoresBody.innerHTML = ``
+        getHiScores()
+    }
 })
 
 
-// 
+// call hi-score from database
 const getHiScores = () => {
     fetch('http://localhost:3000/api/v1/games')
         .then(res => res.json())
         .then(allGames => {
+            // console.log(allGames)
+
             // all game objects from database
             allGames.forEach(game => {
                 // for each player that played create a table row 
                 const playerHiscorreRow = document.createElement('tr')
+                playerHiscorreRow.innerHTML = ``
                 // add inform wihtin that table row
-                playerHiscorreRow.innerHTML += `
-               <td> ${game.id}</td>
-               <td> ${game.score}</td>
+                scoresBody.innerHTML += `
+               <td> ${game.user.username}</td>
+               <td> ${game.difficulty}</td>
+               <td> ${game.high_score}</td>
             `
+                // hiScoreTbl.appendChild(playerHiscorreRow)
             });
         })
 }
+
+const rando = (x) => {
+    for (let index = 0; index < x; index++) {
+        keySequenceArray.push(Math.floor(Math.random() * 4) + 37)
+    }
+}
+rando(4)
+console.log(keySequenceArray)
+//User must be logged in
+document.addEventListener('keydown', e => {
+    if (e.keyCode === keySequenceArray[0]) {
+        console.log('correct')
+        keySequenceArray.shift()
+    } else {
+        console.log('smh')
+        keySequenceArray = []
+        rando(4)
+    }
+
+    // console.log(rando(4), keySequenceArray)
+})
